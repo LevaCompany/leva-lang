ba1c4358b46c9bdd0cfb22be251d6a5b042aff09
Leva <levbulychou1234567890@outlook.com>
2022-10-25 23:08:54 +0300

    Create review.vue

---
+++ review.vue
@@ -0,0 +1,10 @@
+<Leva-Proceres>
+ <Lang-Title>Hello, world!</Lang-Title>
+ <conenct href="mix.css" src="mix.css">
+ <Lang-Body>
+  <Lang-Boxing>
+   <messageBox>Hello, world!</messageBox>
+   <pause>
+  </Lang-Boxing>
+ </Lang-Body>
+</Leva-Proceres>
